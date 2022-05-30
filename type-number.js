283bb230b137a82813e865f39f54850f2401d56a
carol-rdg <103288594+carol-rdg@users.noreply.github.com>
2022-05-30 13:12:39 -0300

    Add files via upload

---
+++ type-number.js
@@ -0,0 +1,5 @@
+const numeroUm = 3;
+const numeroDois = 2;
+
+const operacaoMatematica = numeroUm * numeroDois;
+console.log(operacaoMatematica)
---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura";
+const StringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Ana Carolina";
+
+//concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string OU template literal
